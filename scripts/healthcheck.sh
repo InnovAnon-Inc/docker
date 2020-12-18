aa35194ebf2a3753556e53b21746d79045579e0e
CircleCI <InnovAnon-Inc@protonmail.com>
2020-12-18 18:02:37 +0000

    [CircleCI] updating subtree

--- scripts/healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-set -euxo pipefail
-(( ! UID )) # TODO ?
-(( ! $# ))
-
-pgrep cpuminer
-#curl --fail http://localhost:4048 || exit 1
-nmap -p 4048 localhost | grep '^4048/tcp *open'
-
